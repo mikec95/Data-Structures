04b4df3892c298d50bd0f9b72a8e1caf12d6239d
Mike Carbonari <mikecarbonari1@gmail.com>
2021-03-22 17:19:53 -0400

    Create new destructuring objects file. Include object with nested object for destructuring during the exercise

---
+++ destructuring-objects.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Object with data used for exercises below.
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
