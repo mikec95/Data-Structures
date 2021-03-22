7d3cbf08067ed1305b0c3d548d884bc84a38dd23
Mike Carbonari <mikecarbonari1@gmail.com>
2021-03-22 18:18:28 -0400

    destructure nested objects

--- destructuring-objects.js
+++ destructuring-objects.js
@@ -26,3 +26,52 @@ const restaurant = {
     },
   },
 };
+
+// When destructuring arrays, the order does not matter,
+// however the variable names must match the object's property names exactly
+const { name, categories, openingHours } = restaurant;
+
+// In order to create variables with new names to destructure to,
+// include a colon and the new variable name, like so:
+const {
+  name: restuarantName,
+  categories: cats,
+  openingHours: hours,
+} = restaurant;
+console.log(restuarantName, cats, hours);
+
+// Include default values as well, to avoid undefined.
+// This can be useful when using a variable name
+// that does not match any of the objects property names.
+const { menu = [], starterMenu: apps } = restaurant;
+
+// Instead of 'undefined', for the menu (since menu does not exist in restuarant),
+// an empty array will appear.
+console.log(menu, apps);
+
+/** Mutating variables from destructured objects */
+let a = 100;
+let b = 200;
+
+// Create object with property names === above variables.
+const obj = { a: 5, b: 10 };
+
+// Destructure, however wrap statement in parens to avoid Syntax Error.
+// NOTE: this is necessary when mutating variables by destructuring.
+({ a, b } = obj);
+
+// Variables from above will be mutated into 5 and 10.
+console.log(a, b);
+
+/** Destructuring from nested objects */
+// The restuarant object includes a nested object for the opening hours.
+// The opening hours has already been destructured and saved to an openingHours variable. (ln 32)
+// Start by destructuring from openingHours for Saturday:
+const { sat } = openingHours;
+
+// prints saturday properties and values.
+console.log(sat);
+
+// Destructure from Saturday.
+const { open } = sat;
+console.log(open);
