559216ca782555a08861380b2551ab6f9621a95f
mikec95 <mikecarbonari1@gmail.com>
2021-04-21 15:13:46 -0500

    Update documentation

--- rest-pattern.js
+++ rest-pattern.js
@@ -9,10 +9,9 @@
 const [a, b, c] = [1, 2, ...[3, 4, 5]];
 console.log(a, b, c);
 
-// REST, used to pack elements into an array. Used on left side of declaration
+// REST, used to pack 'rest' of elements into separate array. Used on left side of declaration
 // OUTPUT: Prints "1, 2, 3, [4, 5, 6]".
 // First 3 elements are destructured into a, b, c.
 // 4, 5, 6 are packed into others using REST
 const [a, b, c, ...others] = [1, 2, 3, 4, 5, 6];
 console.log(a, b, c, others);
-
