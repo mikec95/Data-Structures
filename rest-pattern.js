fd366ebdddbe8f449ba7ebb5d7bfb8a0f52e549e
mikec95 <mikecarbonari1@gmail.com>
2021-04-21 16:19:03 -0500

    clean up code

--- rest-pattern.js
+++ rest-pattern.js
@@ -6,15 +6,15 @@
 // SPREAD, used to unpack an array. Used on right side of declaration.
 // OUTPUT: Prints "1, 2, 3, 4, 5" all as one string.
 // The [3, 4, 5] were unpacked with the spread operator.
-let [a, b, c] = [1, 2, ...[3, 4, 5]];
+const [a, b, c] = [1, 2, ...[3, 4, 5]];
 console.log(a, b, c);
 
 // REST, used to pack 'rest' of elements into separate array. Used on left side of declaration
 // OUTPUT: Prints "1, 2, 3, [4, 5, 6]".
 // First 3 elements are destructured into a, b, c.
 // 4, 5, 6 are packed into others using REST
-[a, b, c, ...others] = [1, 2, 3, 4, 5, 6];
-console.log(a, b, c, others);
+const [x, y, z, ...others] = [1, 2, 3, 4, 5, 6];
+console.log(x, y, z, others);
 
 /************************************************************* */
 
@@ -95,9 +95,9 @@ add(11111, 0, 0, 3);
 // We can also use the add function on an array
 // First, destructure array into variables with SPREAD operator
 // Pass variables into add function
-const x = [23, 5, 7];
-[a, b, c] = [...x];
-add(a, b, c);
+const nums = [23, 5, 7];
+const [foo, bar, baz] = [...nums];
+add(foo, bar, baz);
 
 /************************************************************* */
 
@@ -107,5 +107,5 @@ add(a, b, c);
 // Destructure ingredients array first into main variable, and then
 // destructure the REST of them into others variable.
 const ingredients = ["Cheese", "Pineapple", "Bacon", "olives", "spinach"];
-var [main, ...others] = [...ingredients];
-console.log(main, others);
+const [main, ...otherIngs] = [...ingredients];
+console.log(main, otherIngs);
